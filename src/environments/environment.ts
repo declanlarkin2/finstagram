6f27e9c6fe95f249605a4c9056d6cba767f47a80
declanlarkin2 <declan.larkin21@icloud.com>
2023-12-07 13:55:16 +0000

    FI-008: Updating env config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
-import * as dotEnv from 'dotenv';
-dotEnv.config();
 export const environment = {
-  translationKey: process.env.TRANSLATION_KEY || '',
+  translationKey:
+    process.env.TRANSLATION_KEY || '32d02d5ea4ac47f580f8cf14dc6e0837',
 };
